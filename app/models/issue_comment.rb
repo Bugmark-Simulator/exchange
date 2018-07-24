4120884796ec61905e58b0a3fbf703aeee38a935
Vinod <vahuja@unomaha.edu>
2018-07-24 16:14:42 -0500

    Create issue_comment.rb file
    Defining issue comment class in exchange

---
+++ app/models/issue_comment.rb
@@ -0,0 +1,11 @@
+class Issue_Comment < ApplicationRecord
+end
+
+# == Schema Information
+#
+# Table name: Work_queues
+  # id SERIAL primary key not null,
+  # user_uuid character varying not null,
+  # user_name character varying not null,
+  # comment text not null,
+  # comment_date timestamp not null
