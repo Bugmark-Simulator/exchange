2e0bd44af00f5fd7113068a289eed87dcef32851
Georg J.P. Link <linkgeorg@gmail.com>
2018-09-13 15:17:36 -0500

    create tables for simulation when setting up the exchange tables (#5)
    * add simulation tables during db:migrate
    
    * attempt to fix error with rails created db columns
    
    * fix table creation, do not add timestamps

---
+++ db/migrate/20170000000000_create_simulation_tables.rb
@@ -0,0 +1,30 @@
+class CreateSimulationTables < ActiveRecord::Migration[5.1]
+  def change
+
+    create_table :work_queues do |t|
+      t.string   :user_uuid
+      t.string   :issue_uuid
+      t.string   :task
+      t.datetime :added_queue
+      t.integer  :position
+      t.datetime :removed
+      t.datetime :completed
+      t.datetime :startwork
+      t.boolean  :updated_issue, default: false
+    end
+    add_index :work_queues, :user_uuid
+    add_index :work_queues, :issue_uuid
+    add_index :work_queues, :completed
+
+    create_table :issue_comments do |t|
+      t.string   :issue_uuid
+      t.string   :user_uuid
+      t.string   :user_name
+      t.string   :comment
+      t.datetime :comment_date
+      t.datetime :comment_delete
+    end
+    add_index :issue_comments, :issue_uuid
+
+  end
+end
